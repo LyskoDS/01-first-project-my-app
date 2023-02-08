e30e3611a37032e1dc3950260ac36a3352f5283e
LyskoDS <lysko_dima@mail.ru>
2023-02-08 23:25:59 +0300

    Add project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Technologies />
+      </div>
+  );
+}
+
+export default App;
