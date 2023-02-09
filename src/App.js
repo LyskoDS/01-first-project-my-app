eba52e86d76cf7d073d51a83bf22e60768b7eac0
LyskoDS <lysko_dima@mail.ru>
2023-02-09 23:28:31 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies.js';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-        </div>
-    );
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/400px-Vanamo_Logo.svg.png'/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className='content'>Main content</div>
+        </div>);
 }
 
 export default App;
