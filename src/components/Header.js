4f40b57002286ea87db650eb9517fa0c75842f5d
LyskoDS <lysko_dima@mail.ru>
2023-02-13 20:24:05 +0300

    changed CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
+import s from './Header.module.css';
+
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img
             src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/400px-Vanamo_Logo.svg.png'/>
     </header>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,24 +1,32 @@
+import s from './Profile.module.css';
+
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
+        <div>
+            <img
+                src='https://kartinkin.net/uploads/posts/2022-02/1645929650_30-kartinkin-net-p-kartinki-mototsiklistov-38.jpg'/>
+        </div>
         <div>
             <img src='https://cs13.pikabu.ru/avatars/3194/x3194670-1190804622.png'/>
         </div>
-        <div>
+        <div className={s.item}>
             ava + description
         </div>
-        <div>
+        <div className={s.item}>
             My posts
-            <div>
+            <div className={s.item}>
                 New post
             </div>
         </div>
-        <div>
+        <div className={s.posts}>
+        <div className={s.item}>
             post 1
         </div>
-        <div>
+        <div className={s.item}>
             post 2
         </div>
     </div>
+</div>
 }
 
 export default Profile;
