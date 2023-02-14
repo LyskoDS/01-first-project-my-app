e758932db99bd674b8386e1cb29c841eddf8a1b2
LyskoDS <lysko_dima@mail.ru>
2023-02-14 23:18:43 +0300

    ...

--- src/components/Profile/MyPosts/Post/Post.js
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -2,11 +2,11 @@ import s from './Post.module.css';
 
 const Post = (props) => {
     return (
-        <div>
-            <div className={s.item}>
-                <img src='https://i.pinimg.com/originals/d8/be/ae/d8beaedfd933509beaf3202d156491aa.jpg'/>
-                {props.message}
-                {props.likesCount}
+        <div className={s.item}>
+            <img src='https://i.pinimg.com/originals/d8/be/ae/d8beaedfd933509beaf3202d156491aa.jpg'/>
+            {props.message}
+            <div>
+                <span>like</span> {props.likesCount}
             </div>
         </div>
     )
