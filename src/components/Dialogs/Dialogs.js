ad9c7759dd65badd8b81f152051abe3aef7892de
LyskoDS <lysko_dima@mail.ru>
2023-02-16 18:12:20 +0300

    ...

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,23 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Ksenia
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Irina
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are your?</div>
+                <div className={s.message}>I love you</div>
+            </div>
         </div>
     )
 }
