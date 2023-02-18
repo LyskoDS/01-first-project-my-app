0f4c5a6b15d8791c65d94df5cfee955ab52958b3
LyskoDS <lysko_dima@mail.ru>
2023-02-18 22:18:56 +0300

    ...

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,19 +1,26 @@
+import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-    let  post = props.postsData.map(p => <Post message={p.post} likesCount={p.likesCount}/>)
+    let post = props.postsData.map(p => <Post message={p.post} likesCount={p.likesCount}/>);
+
+    let newPost = React.createRef();
+    let addPost = () => {
+        let text = newPost.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.item}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPost}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
